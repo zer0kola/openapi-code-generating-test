18f18a45595e0c186327282847067e2f7c46188d
zer0kola <zer0kola321@gmail.com>
2024-11-02 23:31:36 +0900

    refactor: 파일 분리, zod 생성 테스트

--- openapi-types/codegen.ts
+++ openapi-types/codegen.ts
@@ -2,16 +2,16 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import { OpenAPIV3 } from "openapi-types";
 import SwaggerParser from "@apidevtools/swagger-parser";
-import { generateTypeDefinition, generateZodSchemas } from "./utils";
+import { generateZodSchemas, parseAndGenerateFiles, formatCode } from "./utils";
 
 const URL = "https://petstore3.swagger.io/api/v3/openapi.json";
 const TYPE_PATH = path.resolve(__dirname, "types");
 const ZOD_PATH = path.resolve(__dirname, "zod");
 
 /**
- * 코드 생성
+ * 디렉토리 생성, 파일 생성
  */
-const codeGenerate = async () => {
+const generateFiles = async () => {
   try {
     // swagger parser를 사용해 OpenAPI 문서 파싱
     const api = (await SwaggerParser.parse(URL)) as OpenAPIV3.Document;
@@ -26,24 +26,20 @@ const codeGenerate = async () => {
       fs.mkdirSync(ZOD_PATH, { recursive: true });
     }
 
-    let typeDefinitions = "// 자동 생성된 타입 정의\n\n";
+    const typeDefinitions = await parseAndGenerateFiles(api);
+    // 타입 정의 포맷팅
+    const formattedTypes = await formatCode(typeDefinitions);
 
-    // OAS의 components.schemas 추출
-    const schemas = api.components?.schemas;
-    if (schemas) {
-      // 먼저 모든 타입 선언을 수집
-      Object.entries(schemas).forEach(([name, schema]) => {
-        typeDefinitions += generateTypeDefinition(name, schema as OpenAPIV3.SchemaObject);
-        typeDefinitions += "\n";
-      });
-
-      // 모든 타입을 하나의 파일로 저장
-      fs.writeFileSync(path.join(TYPE_PATH, "pet.d.ts"), typeDefinitions, "utf-8");
-    }
+    const fileName = `Pet.d.ts`;
+    fs.writeFileSync(path.join(TYPE_PATH, fileName), formattedTypes, "utf-8");
 
     // Zod 스키마 생성 및 저장
     const zodSchemas = generateZodSchemas(api);
-    fs.writeFileSync(path.join(ZOD_PATH, "pet.zod.ts"), zodSchemas, "utf-8");
+    // Zod 스키마 포맷팅
+    const formattedZodSchemas = await formatCode(zodSchemas);
+
+    const zodFileName = `Pet.zod.ts`;
+    fs.writeFileSync(path.join(ZOD_PATH, zodFileName), formattedZodSchemas, "utf-8");
 
     console.log("✨ 타입과 Zod 스키마 생성 완료!");
   } catch (error: unknown) {
@@ -51,4 +47,4 @@ const codeGenerate = async () => {
   }
 };
 
-codeGenerate();
+generateFiles();
--- openapi-types/zod/pet.zod.ts
+++ openapi-types/zod/pet.zod.ts
@@ -1,16 +1,64 @@
 // 자동 생성된 Zod 스키마
 
-import { z } from 'zod';
+import { z } from "zod";
 
 export const schemas = {
-  Order: z.object({ "id": z.number().int().optional(), "petId": z.number().int().optional(), "quantity": z.number().int().optional(), "shipDate": z.string().datetime({ offset: true }).optional(), "status": z.enum(["placed","approved","delivered"]).describe("Order Status").optional(), "complete": z.boolean().optional() }),
-  Customer: z.object({ "id": z.number().int().optional(), "username": z.string().optional(), "address": z.array(z.any()).optional() }),
-  Address: z.object({ "street": z.string().optional(), "city": z.string().optional(), "state": z.string().optional(), "zip": z.string().optional() }),
-  Category: z.object({ "id": z.number().int().optional(), "name": z.string().optional() }),
-  User: z.object({ "id": z.number().int().optional(), "username": z.string().optional(), "firstName": z.string().optional(), "lastName": z.string().optional(), "email": z.string().optional(), "password": z.string().optional(), "phone": z.string().optional(), "userStatus": z.number().int().describe("User Status").optional() }),
-  Tag: z.object({ "id": z.number().int().optional(), "name": z.string().optional() }),
-  Pet: z.object({ "id": z.number().int().optional(), "name": z.string(), "category": z.any().optional(), "photoUrls": z.array(z.string()), "tags": z.array(z.any()).optional(), "status": z.enum(["available","pending","sold"]).describe("pet status in the store").optional() }),
-  ApiResponse: z.object({ "code": z.number().int().optional(), "type": z.string().optional(), "message": z.string().optional() }),
+  Order: z.object({
+    id: z.number().int().optional(),
+    petId: z.number().int().optional(),
+    quantity: z.number().int().optional(),
+    shipDate: z.string().datetime({ offset: true }).optional(),
+    status: z
+      .enum(["placed", "approved", "delivered"])
+      .describe("Order Status")
+      .optional(),
+    complete: z.boolean().optional(),
+  }),
+  Customer: z.object({
+    id: z.number().int().optional(),
+    username: z.string().optional(),
+    address: z.array(z.any()).optional(),
+  }),
+  Address: z.object({
+    street: z.string().optional(),
+    city: z.string().optional(),
+    state: z.string().optional(),
+    zip: z.string().optional(),
+  }),
+  Category: z.object({
+    id: z.number().int().optional(),
+    name: z.string().optional(),
+  }),
+  User: z.object({
+    id: z.number().int().optional(),
+    username: z.string().optional(),
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    email: z.string().optional(),
+    password: z.string().optional(),
+    phone: z.string().optional(),
+    userStatus: z.number().int().describe("User Status").optional(),
+  }),
+  Tag: z.object({
+    id: z.number().int().optional(),
+    name: z.string().optional(),
+  }),
+  Pet: z.object({
+    id: z.number().int().optional(),
+    name: z.string(),
+    category: z.any().optional(),
+    photoUrls: z.array(z.string()),
+    tags: z.array(z.any()).optional(),
+    status: z
+      .enum(["available", "pending", "sold"])
+      .describe("pet status in the store")
+      .optional(),
+  }),
+  ApiResponse: z.object({
+    code: z.number().int().optional(),
+    type: z.string().optional(),
+    message: z.string().optional(),
+  }),
   findPetsByStatus_query: z.any(),
   findPetsByTags_query: z.any(),
   getPetById_path: z.any(),
