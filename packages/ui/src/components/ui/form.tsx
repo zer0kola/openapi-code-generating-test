cc45fe147142b02da4d158a6fbd444763ec3fb17
zer0kola <zer0kola321@gmail.com>
2024-12-03 02:34:11 +0900

    fix: build error

--- packages/ui/src/components/ui/form.tsx
+++ packages/ui/src/components/ui/form.tsx
@@ -13,7 +13,7 @@ import {
 } from 'react-hook-form';
 
 import { cn } from '@repo/ui/lib/utils';
-import { Label } from '@repo/ui/components/ui/label';
+import { Label } from './label';
 
 const Form = FormProvider;
 
