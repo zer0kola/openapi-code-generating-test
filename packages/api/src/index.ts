3d8721f2cf1020371681eb028c2904afba93c4fa
zer0kola <zer0kola321@gmail.com>
2024-12-04 23:18:20 +0900

    feat: enhance API package configuration with exports and path mappings

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,5 +2,7 @@ export { PetApi } from '../src/api/pet';
 export { StoreApi } from '../src/api/store';
 export { UserApi } from '../src/api/user';
 
+export * from '../src/types';
+
 export { apiInstance } from '../src/config/api-instance';
 export { type APIResponseData } from '../src/config/types';
